e7f1c122229d6c5b9f886ab12c13e24b0d5e118a
Louis-Rémi Babé <lrbabe@gmail.com>
2016-06-18 23:14:33 +0200

    replace auto-incrementing node ids with random node ids

--- src/actions/_utils.js
+++ src/actions/_utils.js
@@ -1,5 +1,11 @@
-export function getNodeId(prefix) {
-  return `${prefix}_${getNodeId.nextId++}`;
-}
+const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-getNodeId.nextId = 0;
+// Generate an ID with 6 characters suffix
+// Chances of collisions are 1 in 2 billions
+// (we used to use an auto-incrementing id but it was incompatible with loading
+// a snapshot of the state from the network or from a file)
+export function getNodeId(prefix) {
+  return (`${prefix}_XXXXXX`).replace(/X/g, function() {
+    return chars[Math.floor( Math.random() * 36 )];
+  });
+}
